2fd9ef14f7a47bbf9cc8de2919b65b50423facba
m0k1 <moki.forum@gmail.com>
2016-07-28 07:22:14 +0200

    Update

--- budvax.php
+++ budvax.php
@@ -32,17 +32,16 @@ include "includes/header.php";
                                 <h2 class="attraction_heading">ATTRACTIONS <span class="attraction-icon-drop fa fa-angle-down"></span></h2>
 
                                 <div class="attraction_sidebar-content">
-                                    <h3 class="attraction_title"><i class="fa fa-map-marker"></i>Villa Marisol</h3>
                                     <ul class="attraction_location" id="attraction_location">
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.576861,-98.459881" data-title="Carytown Street" data-address="3325 West Cary Street">Carytown Street</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.576922,-98.463700" data-title="Carytown Street" data-address="3325 West Cary Street">Greater Richmond Convention Center</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.577172,-98.457327" data-title="Carytown Street" data-address="3325 West Cary Street">James River Plantations</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.573949,-98.462402" data-title="Carytown Street" data-address="3325 West Cary Street">Jamestown Settlement</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Kings Dominion</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Lewis Ginter Botanical Garden</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Maymont Foundation</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Richmond International Raceway</a></li>
-                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Richmond National Battlefield Park</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="http://www.montenegro360.me/coast/budva/21-library-at-the-citadel.html" data-latlng="36.576861,-98.459881" data-title="Carytown Street" data-address="3325 West Cary Street">Citadel</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="http://www.montenegro360.me/coast/budva/06-street-in-old-town.html" data-latlng="36.576922,-98.463700" data-title="Carytown Street" data-address="3325 West Cary Street">Altstadt (Old Town) Budva</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="http://www.montenegro360.me/coast/budva/01-old-town-and-the-road-to-the-beach-mogren.html" data-latlng="36.577172,-98.457327" data-title="Carytown Street" data-address="3325 West Cary Street">Ballet Dancer Statue</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="https://www.youtube.com/embed/W12hUZPEIpk" data-latlng="36.573949,-98.462402" data-title="Carytown Street" data-address="3325 West Cary Street">Aqua Park</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="https://www.google.rs/maps/place/Mogren+beach,+%D0%A6%D1%80%D0%BD%D0%B0+%D0%93%D0%BE%D1%80%D0%B0/data=!4m2!3m1!1s0x134dd49556b0caf7:0x113574be44fed3f4?sa=X&ved=0ahUKEwieouqVq5XOAhXMNxQKHTbZANEQ8gEIKjAF" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Mogren Beach</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="#" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Budva City Walls</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="https://www.youtube.com/watch?v=P51qbQtxHSg" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Ploce Beach</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="https://en.wikipedia.org/wiki/Podmaine_Monastery" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Podmaine Monastery</a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="https://en.wikipedia.org/wiki/Fort_Kosma%C4%8D" data-latlng="36.5748755,-98.4596874" data-title="Carytown Street" data-address="3325 West Cary Street">Kosmac Fortress</a></li>
                                     </ul>
 
                                 </div>
@@ -57,7 +56,7 @@ include "includes/header.php";
 
                             <div class="attraction_content" id="attraction_content">
 
-                                <h2 class="attraction_content-title">Known as the Mile of Style</h2>
+                                  <h2 class="attraction_content-title">Budva na pjenu od mora</h2>
                                 <br>
                                 <div class="wp_caption aligncenter">
                                     <img src="images/attractions/img-1.jpg" alt="">
@@ -65,7 +64,7 @@ include "includes/header.php";
                                 <br>
                                 <br>
 
-                                <p>Known as the <b>Mile of Style</b>, Carytown offers everything you need to Eat, Shop, and Play. Our sidewalks are lined with unique boutiques, restaurants, specialty shops, spas, and professional services. Enjoy our eclectic collection of award-winning, locally-owned businesses. Spend the day in the area voted “<b>Best Shopping</b> Neighborhood in Virginia” by the readers of Southern <a href="#">Living magazine.</a></p>
+                                <p>Known as the <b>na pjenu od mora</b>, Budva offers everything you need to Eat, Shop, and Play. Our sidewalks are lined with unique boutiques, restaurants, specialty shops, spas, and professional services. Enjoy our eclectic collection of award-winning, locally-owned businesses. Spend the day in the area voted “<b>Best Shopping</b> Neighborhood in Montenegro" by the readers of Southern <a href="#">Living magazine.</a></p>
                                 <br><br>
 
                                 <div class="wp_caption alignleft">
@@ -75,10 +74,10 @@ include "includes/header.php";
                                 <br>
                                 <br>
 
-                                <h2 class="attraction_content-title">Known as the Mile of Style</h2>
+                                <h2 class="attraction_content-title">Known as na pjenu od mora</h2>
                                 <br>
                                 <p>
-                                    Known as the Mile of Style, <b>Carytown</b> offers everything you need to Eat, Shop, and Play. Our sidewalks are lined with unique boutiques, restaurants, specialty shops, spas, and professional services. Enjoy our eclectic collection of award-winning, locally-owned businesses. Spend the day in the area voted “<b>Best Shopping Neighborhood in Virginia</b>” by the readers of <a href="#">Southern Living magazine.</a>
+                                    Known as the na pjenu od mora, <b>Budva</b> offers everything you need to Eat, Shop, and Play. Our sidewalks are lined with unique boutiques, restaurants, specialty shops, spas, and professional services. Enjoy our eclectic collection of award-winning, locally-owned businesses. Spend the day in the area voted “<b>Best Shopping Neighborhood in Montenegro</b>” by the readers of <a href="#">Southern Living magazine.</a>
                                 </p>
 
                             </div>
